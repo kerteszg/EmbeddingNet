6cff68f7d9d4662bef6072ca8a522949c51dafed
kerteszg <kertesz.gabor@gmail.com>
2020-04-24 17:19:05 +0200

    removing unused stuff

--- embedding_net/utils.py
+++ embedding_net/utils.py
@@ -1,29 +1,12 @@
 from sklearn.manifold import TSNE
 import os
-os.environ["TF_KERAS"] = '1'
-import cv2
 import pickle
 import numpy as np
 from matplotlib import pyplot as plt
 import yaml
-from tensorflow.keras import optimizers
+from keras import optimizers
 from .augmentations import get_aug
-
-
-def get_image(img_path, input_shape=None):
-    img = cv2.imread(img_path)
-    if img is None:
-        print('image is not exist ' + img_path)
-        return None
-    if input_shape:
-        img = cv2.resize(
-            img, (input_shape[0], input_shape[1]))
-    return img
-
-def get_images(img_paths, input_shape=None):
-    imgs = [get_image(img_path, input_shape) for img_path in img_paths]
-    return np.array(imgs)
-
+from .data_loader import EmbeddingNetImageLoader
 
 
 def load_encodings(path_to_encodings):
@@ -43,6 +26,7 @@ def plot_tsne(encodings_path, save_plot_dir, show=True):
         xs = tsne_train[np.array(encodings['labels']) == l, 0]
         ys = tsne_train[np.array(encodings['labels']) == l, 1]
         ax.scatter(xs, ys, label=l)
+        #ax.scatter(xs, ys)
         for x, y in zip(xs, ys):
             plt.annotate(l,
                          (x, y),
@@ -56,12 +40,12 @@ def plot_tsne(encodings_path, save_plot_dir, show=True):
         fig.show()
 
     fig.savefig("{}{}.png".format(save_plot_dir, 'tsne.png'))
+    return tsne_train
 
 
-def plot_tsne_interactive(encodings):
+def plot_tsne_interactive(encodings_path):
     import plotly.graph_objects as go
-    if type(encodings) is str:
-        encodings = load_encodings(encodings)
+    encodings = load_encodings(encodings_path)
     labels = list(set(encodings['labels']))
     tsne = TSNE()
     tsne_train = tsne.fit_transform(encodings['encodings'])
@@ -77,8 +61,8 @@ def plot_tsne_interactive(encodings):
                                  mode='markers',
                                  marker=dict(color=color,
                                              size=10),
-                                 text=str(l),
-                                 name=str(l)))
+                                 text=l,
+                                 name=l))
     fig.update_layout(
         title=go.layout.Title(text="t-SNE plot",
                               xref="paper",
@@ -103,95 +87,43 @@ def plot_grapths(history, save_path):
 
         fig.savefig("{}{}.png".format(save_path, k))
 
-def plot_batch_simple(data, targets, class_names):
-        num_imgs = data[0].shape[0]
-        img_h = data[0].shape[1]
-        img_w = data[0].shape[2]
-        full_img = np.zeros((img_h,num_imgs*img_w,3), dtype=np.uint8)
-        indxs = np.argmax(targets, axis=1)
-        class_names = [class_names[i] for i in indxs]
-        
-        for i in range(num_imgs):
-            full_img[:,i*img_w:(i+1)*img_w,:] = data[0][i,:,:,:]
-            cv2.putText(full_img, class_names[i], (img_w*i + 10, 20), cv2.FONT_HERSHEY_SIMPLEX,  
-                        0.5, (0, 255, 0), 1, cv2.LINE_AA)
-        plt.figure(figsize = (20,2))
-        plt.imshow(full_img)
-        plt.show()
 
-    
-def plot_batch(data, targets):
-    num_imgs = data[0].shape[0]
-    it_val = len(data)
-    fig, axs = plt.subplots(num_imgs, it_val, figsize=(
-        30, 50), facecolor='w', edgecolor='k')
-    fig.subplots_adjust(hspace=.5, wspace=.001)
+def parse_net_params(filename='configs/road_signs.yml', verboseOverride=False, verbose=True):
+    params = {}
+    with open(filename, 'r') as ymlfile:
+        cfg = yaml.safe_load(ymlfile)
 
-    axs = axs.ravel()
-    i = 0
-    for img_idx, targ in zip(range(num_imgs), targets):
-        for j in range(it_val):
-            img = cv2.cvtColor(data[j][img_idx].astype(
-                np.uint8), cv2.COLOR_BGR2RGB)
-            axs[i+j].imshow(img)
-            axs[i+j].set_title(targ)
-        i += it_val
-
-    plt.show()
-
-
-def get_optimizer(name, learning_rate):
-    if name == 'adam':
+    learning_rate = cfg['learning_rate']
+    if cfg['optimizer'] == 'adam':
         optimizer = optimizers.Adam(lr=learning_rate)
-    elif name == 'rms_prop':
+    elif cfg['optimizer'] == 'rms_prop':
         optimizer = optimizers.RMSprop(lr=learning_rate)
-    elif name == 'radam':
+    elif cfg['optimizer'] == 'radam':
         from keras_radam import RAdam
         optimizer = RAdam(learning_rate)
     else:
         optimizer = optimizers.SGD(lr=learning_rate)
-    return optimizer
 
-
-def parse_params(filename='configs/road_signs.yml'):
-    with open(filename, 'r') as ymlfile:
-        cfg = yaml.safe_load(ymlfile)
-
-    if 'augmentations_type' in cfg['GENERATOR']:
-        augmentations = get_aug(cfg['GENERATOR']['augmentation_type'], 
-                                cfg['MODEL']['input_shape'])
+    if 'augmentations_type' in cfg:
+        augmentations = get_aug(cfg['augmentation_type'], cfg['input_shape'])
     else:
         augmentations = None
-
-    optimizer = get_optimizer(cfg['TRAIN']['optimizer'], 
-                              cfg['TRAIN']['learning_rate'])
-
-    params_dataloader = cfg['DATALOADER']
-    params_generator = cfg['GENERATOR']
-    params_model = cfg['MODEL']
-    params_train = cfg['TRAIN']
-    params_save_paths = cfg['SAVE_PATHS']
-    params_encodings = cfg['ENCODINGS']
-
-    params_generator['input_shape'] = params_model['input_shape']
-    params_train['optimizer'] = optimizer
-    params_generator['augmentations'] = augmentations
-
-    params = {'dataloader' : params_dataloader,
-              'generator' : params_generator,
-              'model' : params_model,
-              'train' : params_train,
-              'save_paths': params_save_paths,
-              'encodings' : params_encodings}
-
-    if 'SOFTMAX_PRETRAINING' in cfg:
-        params_softmax = cfg['SOFTMAX_PRETRAINING']
-        params_softmax['augmentations'] = augmentations
-        params_softmax['input_shape'] = params_model['input_shape']
-        softmax_optimizer = get_optimizer(cfg['SOFTMAX_PRETRAINING']['optimizer'], 
-                              cfg['SOFTMAX_PRETRAINING']['learning_rate'])
-        params_softmax['optimizer'] = softmax_optimizer
-        params['softmax'] =  params_softmax
         
+    if 'verbose' not in cfg.keys():
+        cfg['verbose'] = True
+    if verboseOverride:
+        cfg['verbose'] = verbose
+        
+    params = {k: v for k, v in cfg.items() if k not in ['optimizer']}
+    
+    if 'dataset_path' in cfg:
+        params['loader'] = EmbeddingNetImageLoader(cfg['dataset_path'],
+                                                   input_shape=cfg['input_shape'],
+                                                   min_n_obj_per_class=cfg['min_n_obj_per_class'],
+                                                   select_max_n_obj_per_class = cfg['select_max_n_obj_per_class'], 
+                                                   max_n_obj_per_class=cfg['max_n_obj_per_class'],
+                                                   augmentations=augmentations,
+                                                   verbose=cfg['verbose'])
 
+    params['optimizer'] = optimizer
     return params
